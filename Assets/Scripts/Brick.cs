90b510bca42b5be8637af0dc854ebb4a28447240
aurepuertadev <aurepuertadev@gmail.com>
2022-09-17 16:30:22 +0200

    Created basic project structure
    Created main needed files and folders

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour
+{
+    [SerializeField] int hitsToBreak;
+    [SerializeField] int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+
+    public float Speed => _speed;
+
+    private Rigidbody _rigidbody;
+    
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
